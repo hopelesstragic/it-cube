38c7feb779baa2097a2d92f3835d85645a703896
hopeless.tragic <120760285+hopelesstragic@users.noreply.github.com>
2023-01-10 13:17:38 +0300

    Add files via upload

---
+++ test10yan/ilya korshunov/main1.py
@@ -0,0 +1,9 @@
+#задача 1
+yo=int(input("Введите любой год"))
+a=yo%10
+b=yo%100
+if a==0 and b==0:
+   print("YES")
+else:
+   print("NO")
+
---
+++ test10yan/ilya korshunov/main4.py
@@ -0,0 +1,25 @@
+#задача 4
+num=int(input("Введите число"))
+if num==1:
+    print("I")
+if num==2:
+    print("II")
+if num==3:
+    print("III")
+if num==4:
+    print("IV")
+if num==5:
+    print("V")
+if num==6:
+    print("VI")
+if num==7:
+    print("VII")
+if num==8:
+    print("VIII")
+if num==9:
+    print("IX")
+if num==10:
+    print("X")
+if num>10:
+    print("ошибка")
+
